a33f71ee0b51df6a59f7e8977e8cb6f814ec3eda
RayAguilera1407 <ray.aguilera@duocuc.cl>
2025-06-05 10:47:48 -0400

    Paso 2: Registro del videojuego

---
+++ videojuegos.py
@@ -0,0 +1,28 @@
+#Trabajo práctico.
+
+videojuegos = []
+plataformas = []
+
+while True:
+    print("\n--- MENÚ DE VIEOJUEGOS ---")
+    print("1. Registrar videojuego")
+    print("2. Ver videojuegos")
+    print("3.-Modificar videojuego")
+    print("4. Eliminar videojuego")
+    print("5. Salir")
+
+    opcion = input("Seleccione una opción (1-5): ")
+
+    if opcion == "1":
+        pass
+    elif opcion == "2":
+        pass
+    elif opcion == "3":
+        pass
+    elif opcion == "4":
+        pass
+    elif opcion =="5":
+        print("Saliendo del programa.")
+        break
+    else:
+        print("Opción inválida.")
